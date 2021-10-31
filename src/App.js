f14c3c4aae4dfb3510983b3454440f454774e4fb
Saravanan K <53828581+saravanankish@users.noreply.github.com>
2021-10-31 20:18:14 +0530

    Router Changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Login from './components/Login/Login';
 import "tailwindcss/tailwind.css";
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Signup from './components/Signup/Signup';
 import ChatScreen from './components/ChatScreen/ChatScreen';
 
 function App() {
   return (
     <div className="App h-screen">
-      <p className="lg:hidden text-center font-semibold text-2xl mt-28">See in desktop for full view</p>
-      <div className="App hidden lg:block">
+      <p className="md:hidden text-center font-semibold text-2xl mt-28">See in desktop for full view</p>
+      <div className="App hidden md:block">
         <Router>
           <Switch>
             <Route exact path="/login">
